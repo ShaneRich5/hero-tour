c3a4598e96149dfed4fec3e554fe2e690226628a
Shane Richards <shane.richards121@gmail.com>
2016-12-13 21:05:37 -0500

    input decorator on hero

--- src/app/heroes/hero-detail.component.ts
+++ src/app/heroes/hero-detail.component.ts
@@ -1,11 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
+  template: `
+		<div *ngIf="hero">
+			<h2>{{hero.name}} details!</h2>
+			<div><label>id: </label>{{hero.id}}</div>
+			<div>
+				<label>name: </label>
+				<input [(ngModel)]="hero.name" placeholder="name"/>
+			</div>
+		</div>
+		`,
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
+	@Input()
+	hero: Hero;
 
   constructor() { }
 
