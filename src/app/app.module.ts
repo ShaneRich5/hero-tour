3677aa395debf8db7a6e66af97ff01a387ed8c0b
Shane Richards <shane.richards121@gmail.com>
2016-12-15 03:02:50 -0500

    CRUD functionallity with mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpModule }     from '@angular/http';
 
 import { AppRoutingModule }     from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './heroes/hero-detail.component';
 import { HeroListComponent }    from './heroes/hero-list.component';
@@ -22,6 +26,7 @@ import { HeroService }          from './heroes/hero.service';
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [
