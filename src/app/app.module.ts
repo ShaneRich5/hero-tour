1f7625aa203be23727888e543c538350d2b76654
Shane Richards <shane.richards121@gmail.com>
2016-12-14 19:01:02 -0500

    updated dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule }       from '@angular/core';
 import { FormsModule }    from '@angular/forms';
 import { HttpModule }     from '@angular/http';
 
+import { AppRoutingModule }     from './app-routing.module';
+
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './heroes/hero-detail.component';
 import { HeroListComponent }    from './heroes/hero-list.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroService }          from './heroes/hero.service';
-
-import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
